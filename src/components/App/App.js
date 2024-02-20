ec120c4f5816185a8e317802aa7fe3035965bac7
Krisztian Ivan <krisztian.ivan@gmail.com>
2024-02-20 09:36:54 +0000

    added SearchBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,17 @@
-import './App.css';
+import React from "react";
+
+import SearchBar from "../SearchBar/SearchBar";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
       <div className="App">
-        <div className="App-playlist">
-          
-        </div>
+        <SearchBar />
+        <div className="App-playlist"></div>
       </div>
     </div>
   );
